fe4736f6bcf49d7e907101cce8b75a818688dc41
Danny Thomas <dthomas@blackboard.com>
2013-05-16 14:59:33 -0400

    Add tests

---
+++ src/test/java/blackboard/plugin/hayabusa/command/PackageSanityTest.java
@@ -0,0 +1,7 @@
+package blackboard.plugin.hayabusa.command;
+
+import com.google.common.testing.AbstractPackageSanityTests;
+
+public class PackageSanityTest extends AbstractPackageSanityTests
+{
+}
---
+++ src/test/java/blackboard/plugin/hayabusa/provider/PackageSanityTest.java
@@ -0,0 +1,7 @@
+package blackboard.plugin.hayabusa.provider;
+
+import com.google.common.testing.AbstractPackageSanityTests;
+
+public class PackageSanityTest extends AbstractPackageSanityTests
+{
+}
