51a7fdfc037ab1f88e6a05b818f788b2a86aa115
Guoyuli <li.guoyu@blackboard.com.cn>
2013-06-27 17:05:44 +0800

    add description

--- src/main/webapp/js/hayabusa-main.js
+++ src/main/webapp/js/hayabusa-main.js
@@ -7,6 +7,7 @@ jQuery( function()
   // /provider/commands
   jQuery.getJSON( "/webapps/bb-hayabusa-BBLEARN/execute/provider/commands", function( data )
   {
+    parse( data.commands );
     jQuery( "#lightboxInput" ).autocomplete(
     {
         autoFocus : true,
@@ -18,10 +19,7 @@ jQuery( function()
           matchedJSON = ui.item;
           parent.frames['content'].location.href = getUri();
         }
-    } ).data( 'ui-Autocomplete' )._renderItem = function( ul, item )
-    {
-      return jQuery( "<li>" ).append( "<a>" + item.category.name + " - " + item.title + "</a>" ).appendTo( ul );
-    }
+    } );
   } )
 } );
 
@@ -29,4 +27,12 @@ function getUri()
 {
   var uri = matchedJSON.uri;
   return uri;
+}
+
+function parse( items )
+{
+  jQuery( items ).each( function()
+  {
+    this.value = this.category.name + ' - ' + this.title;
+  } );
 }
