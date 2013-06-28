b4f03dd7fbe4c6a5cb942ec5c8df1b72c0ea2159
Guoyuli <li.guoyu@cerbibo.com>
2013-06-28 14:53:43 +0800

    course provider

--- src/main/java/blackboard/plugin/hayabusa/config/HayabusaConfiguration.java
+++ src/main/java/blackboard/plugin/hayabusa/config/HayabusaConfiguration.java
@@ -1,16 +1,15 @@
 /*
  * Copyright (c) 2013, Blackboard, Inc. All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
  * following conditions are met:
- *
+ * 
  * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
- *    disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
- *    following disclaimer in the documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Blackboard Inc. nor the names of its contributors may be used to endorse or promote
- *    products derived from this software without specific prior written permission.
- *
+ * disclaimer. 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided with the distribution. 3. Neither the
+ * name of the Blackboard Inc. nor the names of its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
@@ -18,7 +17,7 @@
  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  * BLACKBOARD MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-
  * INFRINGEMENT. BLACKBOARD SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
@@ -27,15 +26,11 @@
 
 package blackboard.plugin.hayabusa.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import blackboard.plugin.hayabusa.provider.ModuleItemProvider;
-import blackboard.plugin.hayabusa.provider.Provider;
-import blackboard.plugin.hayabusa.provider.SendEmailProvider;
+import blackboard.plugin.hayabusa.provider.*;
 
 /**
  * Spring configuration for the Hayabua project.
@@ -54,11 +49,16 @@ public class HayabusaConfiguration extends WebMvcConfigurerAdapter
   {
     return new ModuleItemProvider();
   }
-  
+
   @Bean
   public Provider sendEmailProvider()
   {
     return new SendEmailProvider();
   }
 
+  @Bean
+  public Provider courseProvider()
+  {
+    return new CourseProvider();
+  }
 }
